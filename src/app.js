eb035df88d057497a589407cc9c4820ff0d365ab
Arthi25 <arthur.stoffels25@gmail.com>
2023-11-14 11:56:05 +0100

    add : handler 3 languages

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ function isEqualsToNameUpperCase(str) {
 
 function isArrayAndHaveTwoNames(arr) {
   let stringToReturn = 'Hello, ';
+  let concat;
+  let language;
+  let countLanguage;
+  let newArray = [];
   const upperCaseArray = arr.filter(isEqualsToNameUpperCase);
   const lowerCaseArray = arr.filter((n) => !isEqualsToNameUpperCase(n));
 
@@ -34,6 +38,68 @@ function isArrayAndHaveTwoNames(arr) {
     stringToReturn += ` AND HELLO ${upperCaseArray[0]} !`;
   }
 
+  arr.forEach((n, index) => {
+    if (n.length === 2) {
+      countLanguage++;
+      if (n === 'fr' || n === 'nl' || n === 'en') {
+        language = n;
+        arr.splice(index, 1);
+      }
+    } else {
+      newArray.push(n);
+    }
+  })
+  // if more than a language
+  if (countLanguage > 1){
+    return false;
+  }
+
+  // if language isn't a save language
+  if (countLanguage === 1 && language === ' '){
+    return false;
+  }
+  
+  if (language === 'fr') {
+    stringToReturn = 'Bonjour, ';
+    concat = 'et ';
+    arr.forEach((n, index) => {
+      console.log(n);
+      if (arr.length === 1){
+        stringToReturn += `${n}.`
+      } else if (index === arr.length - 1) {
+        stringToReturn += `${concat}${n}.`;
+      } else {
+        stringToReturn += `${n} `;
+      }
+    });
+  } else if (language === 'nl') {
+    stringToReturn = 'Hallo, ';
+    concat = 'en ';
+    arr.forEach((n, index) => {
+      console.log(n);
+      if (arr.length === 1){
+        stringToReturn += `${n}.`
+      } else if (index === arr.length - 1) {
+        stringToReturn += `${concat}${n}.`;
+      } else {
+        stringToReturn += `${n} `;
+      }
+    });
+  } else if (language === 'en') {
+    stringToReturn = 'Hello, '
+    concat = 'and '; 
+    arr.forEach((n, index) => {
+      console.log(n);
+      if (arr.length === 1){
+        stringToReturn += `${n}.`
+      } else if (index === arr.length - 1) {
+        stringToReturn += `${concat}${n}.`;
+      } else {
+        stringToReturn += `${n} `;
+      }
+    });
+  }
+  
   return stringToReturn;
 }
 
--- src/app.test.js
+++ src/app.test.js
@@ -55,3 +55,34 @@ test('should return false', () => {
   const result = greet(['Amy', 'BRIAN', 'CHARLOTTE']);
   expect(result).toEqual(false);
 });
+
+// 7
+test('should return "Bonjour, Marie."', () => {
+  const result = greet(['Marie', 'fr']);
+  expect(result).toEqual('Bonjour, Marie.');
+});
+
+test('should return "Bonjour, Marie et Jeanne."', () => {
+  const result = greet(['Marie', 'Jeanne', 'fr']);
+  expect(result).toEqual('Bonjour, Marie et Jeanne.');
+});
+
+test('should return "Hello, Marie."', () => {
+  const result = greet(['Marie', 'en']);
+  expect(result).toEqual('Hello, Marie.');
+});
+
+test('should return "Hello, Marie and Jeanne."', () => {
+  const result = greet(['Marie', 'Jeanne', 'en']);
+  expect(result).toEqual('Hello, Marie and Jeanne.');
+});
+
+test('should return "Hallo, Marie."', () => {
+  const result = greet(['Marie', 'nl']);
+  expect(result).toEqual('Hallo, Marie.');
+});
+
+test('should return "Hallo, Marie en Jeanne."', () => {
+  const result = greet(['Marie', 'Jeanne', 'nl']);
+  expect(result).toEqual('Hallo, Marie en Jeanne.');
+});
